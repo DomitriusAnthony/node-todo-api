167bca48ac86ed88d1cf70d0732208c336668283
Domitrius Clark <domitrius.anthony@hotmail.com>
2017-07-12 10:56:59 -0400

    Initial commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,45 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+var obj = new ObjectID();
+console.log(obj);
+
+// object destructuring lets you pull out properties from on object, creating variables
+
+// EX: 
+// var user = {name: "Domitrius", age: 24}
+// var {name} = user;
+
+
+
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+	if (err) {
+		return console.log("Unable to connect to MongoDB server");
+	}
+	console.log("Connected to MongoDB server");
+
+	// db.collection('Todos').insertOne({
+
+	// }, (err, result) => {
+	// 	if (err) {
+	// 		return console.log("Unable to insert ToDo", err);
+	// 	}
+
+	// 	console.log(JSON.stringify(result.ops, undefined, 2));
+	// })
+
+	// db.collection('Users').insertOne({
+	// 	name: "Domitrius",
+	// 	age: 24,
+	// 	location: "Philadelphia"
+	// }, (err, result) => {
+	// 	if (err) {
+	// 		return console.log("Unable to insert ToDo", err);
+	// 	}
+
+	// 	console.log(result.ops[0]._id.getTimestamp());
+	// });	
+
+	db.close();
+});
